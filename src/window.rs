f1ca5b646277ee2f42cd51a34abf6a4689903686
Emmanuele Bassi <ebassi@gnome.org>
2022-05-05 16:03:03 +0100

    Restructure the background and foreground colors
    Try to improve the legibility of the text and UI elements by splitting
    the foreground between main window and playlist panel, and using the
    target color of each gradient.
    
    Fixes: #97

--- src/window.rs
+++ src/window.rs
@@ -884,12 +884,17 @@ impl Window {
                 // The color chosen depends on the linear gradient we use in the
                 // style, so remember to change this when changing the main-window
                 // CSS class
-                let fg_color =
-                    if utils::is_color_dark(&bg_colors[0]) || utils::is_color_dark(&bg_colors[1]) {
+                let fg_main_color =
+                    if utils::is_color_dark(&bg_colors[0]) || utils::is_color_dark(&bg_colors[2]) {
                         gdk::RGBA::parse("#ffffff").unwrap()
                     } else {
                         gdk::RGBA::parse("rgba(0, 0, 0, 0.8)").unwrap()
                     };
+                let fg_playlist_color = if utils::is_color_dark(&bg_colors[1]) {
+                    gdk::RGBA::parse("#ffffff").unwrap()
+                } else {
+                    gdk::RGBA::parse("rgba(0, 0, 0, 0.8)").unwrap()
+                };
 
                 let mut css = String::new();
 
@@ -931,7 +936,14 @@ impl Window {
                     ));
                 }
 
-                css.push_str(&format!("@define-color foreground_color {};", fg_color));
+                css.push_str(&format!(
+                    "@define-color foreground_main_color {};",
+                    fg_main_color
+                ));
+                css.push_str(&format!(
+                    "@define-color foreground_playlist_color {};",
+                    fg_playlist_color
+                ));
 
                 imp.provider.load_from_data(css.as_bytes());
                 if !imp.main_stack.has_css_class("main-window") {
